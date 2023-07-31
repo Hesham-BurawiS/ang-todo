027f420040c9bc8e1c0eac4b3d3d8f261674e27f
Hesham-BurawiS <hesham@burawi.com>
2023-07-31 13:48:55 +0300

    Initial?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import {ReactiveFormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OrdersComponent } from './component/orders/orders.component';
+import { OrderComponent } from './component/order/order.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    OrderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    RouterModule.forRoot([
+      { path: '', component: OrdersComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/order.ts
@@ -0,0 +1,22 @@
+export interface Order {
+  id: number;
+  description: string;
+
+}
+
+export let orders = [
+  {
+    id:0,
+    description: "test"
+  },
+  {
+    id:1,
+    description: "test"
+  },
+  {
+    id:2,
+    description: "test"
+  }
+];
+
+export let orderss = []
